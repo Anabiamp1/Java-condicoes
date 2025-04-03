853d3da88803f89795537f3f71aeb16d983a973b
Ana Beatriz M P Ramos <anabeatrizmirandapiveta@gmail.com>
2025-04-03 17:34:25 -0300

    Java-velocidade

---
+++ ex08.js
@@ -0,0 +1,6 @@
+var vel = 78
+console.log(`A Velocidade de seu carro é de ${vel} km/h`)
+if (vel > 60) {
+    console.log(`Você ultrapassou o limite de velocidade! Multa!`)
+}
+console.log(`Dirija  com segurança.`)
